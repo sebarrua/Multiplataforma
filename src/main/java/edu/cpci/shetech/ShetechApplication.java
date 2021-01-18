8a2c34f0e91889f0fc572993da4b3b7b415fdaf7
Arrua Sebastian <sarrua@gtisa.com.ar>
2021-01-18 12:00:06 -0300

    Spring Initializr

---
+++ src/main/java/edu/cpci/shetech/ShetechApplication.java
@@ -0,0 +1,13 @@
+package edu.cpci.shetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShetechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cpci/shetech/ShetechApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpci.shetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
