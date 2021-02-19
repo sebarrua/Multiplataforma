ecada1fbc7f5441b9be99d3c06499db2db05aed8
sebarrua <arrua.sebastian@gmail.com>
2021-02-18 22:16:04 -0300

    18/2/2021 Estructura del Proyecto

--- src/main/java/edu/cpci/shetech/ShetechApplication.java
+++ src/main/java/edu/cpci/shetech/ShetechApplication.java
@@ -2,7 +2,11 @@ package edu.cpci.shetech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@Configuration
 @SpringBootApplication
 public class ShetechApplication {
 
